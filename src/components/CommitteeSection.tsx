6ff3e655d563346f76d2d1211c5ea71d5d8bb9ac
NayanVR <nvradadiya281@gmail.com>
2024-09-19 09:32:34 +0530

    Rewritten entire project

---
+++ src/components/CommitteeSection.tsx
@@ -0,0 +1,118 @@
+import React from "react";
+import {
+  ADVISORY_COMMITTEE,
+  CHIEF_PATRON,
+  CO_CONVENORS,
+  CONFERENCE_CHAIR,
+  CONVENORS,
+  EDITORIAL_TEAM,
+  ORGANIZING_COMMITTEE,
+  TECHNICAL_PROGRAM_COMMITTEE,
+  type Member,
+} from "@/lib/committees";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "./ui/tabs";
+
+const CommitteeSection = () => {
+  return (
+    <section id="committee">
+      <div className="max-w-7xl mx-auto px-6 md:px-12 xl:px-6">
+        <h1
+          className="text-4xl font-bold text-gray-800 dark:text-white md:text-5xl text-center mb-12"
+          id="commitee"
+        >
+          Committee
+        </h1>
+
+        <Tabs defaultValue="chief-patron">
+          <TabsList className="w-full h-max flex justify-between overflow-x-auto no-scrollbar">
+            <TabsTrigger value="chief-patron">
+              <TabTitle title="Chief Patron" />
+            </TabsTrigger>
+            <TabsTrigger value="advisory-committee">
+              <TabTitle title="Advisory Committee" />
+            </TabsTrigger>
+            <TabsTrigger value="technical-program-committee">
+              <TabTitle title="Technical Program Committee" />
+            </TabsTrigger>
+            <TabsTrigger value="conference-chair">
+              <TabTitle title="Conference Chair" />
+            </TabsTrigger>
+            <TabsTrigger value="convenors">
+              <TabTitle title="Convenors" />
+            </TabsTrigger>
+            <TabsTrigger value="organizing-committee">
+              <TabTitle title="Organizing Committee" />
+            </TabsTrigger>
+            <TabsTrigger value="editorial-team">
+              <TabTitle title="Editorial Team" />
+            </TabsTrigger>
+          </TabsList>
+
+          <TabsContent value="chief-patron">
+            <CommitteeTitle title="Chief Patron" />
+            <CommitteeList members={CHIEF_PATRON} />
+          </TabsContent>
+          <TabsContent value="advisory-committee">
+            <CommitteeTitle title="Advisory Committee" />
+            <CommitteeList members={ADVISORY_COMMITTEE} />
+          </TabsContent>
+          <TabsContent value="technical-program-committee">
+            <CommitteeTitle title="Technical Program Committee" />
+            <CommitteeList members={TECHNICAL_PROGRAM_COMMITTEE} />
+          </TabsContent>
+          <TabsContent value="conference-chair">
+            <CommitteeTitle title="Conference Chair" />
+            <CommitteeList members={CONFERENCE_CHAIR} />
+          </TabsContent>
+          <TabsContent value="convenors">
+            <CommitteeTitle title="Convenors" />
+            <CommitteeList members={CONVENORS} />
+            <CommitteeTitle title="Co-Convenors" />
+            <CommitteeList members={CO_CONVENORS} />
+          </TabsContent>
+          <TabsContent value="organizing-committee">
+            <CommitteeTitle title="Organizing Committee" />
+            <CommitteeList members={ORGANIZING_COMMITTEE} />
+          </TabsContent>
+          <TabsContent value="editorial-team">
+            <CommitteeTitle title="Editorial Team" />
+            <CommitteeList members={EDITORIAL_TEAM} />
+          </TabsContent>
+        </Tabs>
+      </div>
+    </section>
+  );
+};
+
+const CommitteeList = ({ members }: { members: Array<Member> }) => {
+  return (
+    <ul className="divide-y divide-gray-200 dark:divide-gray-700">
+      {members.map(({ name, position }, i) => (
+        <li key={i} className="py-4 sm:py-5">
+          <div className="flex flex-col items-center">
+            <p className="text-xl font-medium text-gray-900 dark:text-white">
+              {name}
+            </p>
+            <p className="text-xl text-gray-500 dark:text-gray-400">
+              {position}
+            </p>
+          </div>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+const TabTitle = ({ title }: { title: string }) => {
+  return (
+    <h2 className="text-lg font-semibold px-4 py-2 transition-all">{title}</h2>
+  );
+};
+const CommitteeTitle = ({ title }: { title: string }) => {
+  return (
+    <h2 className="text-2xl font-bold p-4 mt-4 text-center text-primary">
+      {title}
+    </h2>
+  );
+};
+export default CommitteeSection;
---
+++ src/lib/committees.ts
@@ -0,0 +1,208 @@
+export type Member = {
+    name: string;
+    position: string;
+};
+
+export const CHIEF_PATRON: Array<Member> = [
+    {
+        name: "Shri. P. K Taneja",
+        position: "Rtd. IAS",
+    },
+];
+
+export const ADVISORY_COMMITTEE: Array<Member> = [
+    {
+        name: "Prof. (DR.) MANOJ CHOUDHARY",
+        position: "Vice Chancellor, Gati Shakti Vishwavidyalaya (GSV), Vadodara",
+    },
+    {
+        name: "Prof. Karuna Jain",
+        position: "Professor, I.I.T Bombay",
+    },
+    {
+        name: "Prof Ts Dr Humaira Nisar",
+        position: "Professor, Universiti Tunku Abdul Rahman, Malaysia",
+    },
+    {
+        name: "Prof. Meenakshi D'Souza",
+        position: "Professor, IIIT Bangalore",
+    },
+    {
+        name: "Dr. Bhavesh N Gohil",
+        position: "Associate Professor, SVNIT, Surat",
+    },
+    {
+        name: "Mr. Ramesh B. Panchal",
+        position: "Director (Administration), GSFC University, Vadodara",
+    },
+    {
+        name: "Mr. Mahesh Barot",
+        position: "Dy. Director - Administration, GSFC University, Vadodara",
+    },
+    {
+        name: "Mr. Bimal B Bhayani",
+        position: "Professor of Practice cum Advisor, GSFC University, Vadodara",
+    },
+    {
+        name: "Mr. Naren Acharya",
+        position: "Assistant Director, GSFC University, Vadodara",
+    },
+    {
+        name: "Dr. Saurabh Shah",
+        position: "Professor - School of technology, GSFC University, Vadodara",
+    },
+    {
+        name: "Dr. Nishith Parikh",
+        position: "Associate Dean - Academic Affairs, GSFC University, Vadodara",
+    },
+    {
+        name: "Dr. K. Santhosh Kumar",
+        position:
+            "Dean of Student Activities and Alumni Interaction Cell, GSFC University, Vadodara",
+    },
+    {
+        name: "Dr. Devjani Banerjee",
+        position:
+            "Associate Dean-Dept. of Life Sciences, GSFC University, Vadodara",
+    },
+    {
+        name: "Dr. Chetna Parmar",
+        position: "Associate Dean - BBA, GSFC University, Vadodara",
+    },
+    {
+        name: "Dr. Abha Kalaiya",
+        position: "Associate Dean - MBA, GSFC University, Vadodara",
+    },
+    {
+        name: "Mr. A. Srikrishnan",
+        position: "Associate Dean(Fire & EHS), GSFC University, Vadodara",
+    },
+    {
+        name: "Ms. Neha Pujara",
+        position: "Associate Dean of (DOST-AI) Cell, GSFC University, Vadodara",
+    },
+];
+
+export const TECHNICAL_PROGRAM_COMMITTEE: Array<Member> = [
+    {
+        name: "Prof. Amitabh Mishra",
+        position: "Associate Professor, University of West Florida, United States",
+    },
+    {
+        name: "Prof. Maulika Patel",
+        position: "Professor, Head, GCET, VV Vidhyanagar",
+    },
+    {
+        name: "Prof. K. S. Kasiviswanathan",
+        position: "Associate Professor, Indian Institute of Technology Roorkee",
+    },
+    {
+        name: "Prof. Nilesh Bahadure",
+        position: "Professor, GSFC University, Vadodara",
+    },
+    {
+        name: "Dr. Saroj Shekhawat",
+        position: "Associate Professor, GSFC University, Vadodara",
+    },
+    {
+        name: "Dr. Akhilesh Prajapati",
+        position: "Associate Professor, GSFC University, Vadodara",
+    },
+    {
+        name: "Dr. Chandra Has",
+        position: "Associate Professor, GSFC University, Vadodara",
+    },
+];
+
+export const CONFERENCE_CHAIR: Array<Member> = [
+    {
+        name: "Dr. G. R Sinha ",
+        position: "Provost",
+    },
+];
+
+export const CONVENORS: Array<Member> = [
+    {
+        name: "Dr. Sanjukta Bose Goswami",
+        position: "Dean - School of Technology",
+    },
+];
+
+export const CO_CONVENORS: Array<Member> = [
+    {
+        name: "Dr. Jahanvi Bansal",
+        position: "Associate Dean - Research and Development cell",
+    },
+    {
+        name: "Dr. Priya Goel",
+        position: "Assistant Professor, Chemistry",
+    },
+    {
+        name: "Ms. Swati Saxena",
+        position: "Assistant Professor, Computer Science & Engineering",
+    },
+];
+
+export const ORGANIZING_COMMITTEE: Array<Member> = [
+    {
+        name: "Dr. Parag Saxena",
+        position: "Assistant Professor, GSFC University, Vadodara",
+    },
+    {
+        name: "Dr. Naznin Pathan",
+        position: "Assistant Professor, GSFC University, Vadodara",
+    },
+    {
+        name: "Dr. Sindhura G",
+        position: "Assistant Professor, GSFC University, Vadodara",
+    },
+    {
+        name: "Dr. Shilpa Parmar",
+        position: "Assistant Professor, GSFC University, Vadodara",
+    },
+    {
+        name: "Dr. Jignesh Valand",
+        position: "Assistant Professor, GSFC University, Vadodara",
+    },
+    {
+        name: "Ms. Mousam Patel",
+        position: "Assistant Professor, GSFC University, Vadodara",
+    },
+    {
+        name: "Ms. Archana Magre",
+        position: "Assistant Professor, GSFC University, Vadodara",
+    },
+    {
+        name: "Ms. Zalak Kansagara",
+        position: "Assistant Professor, GSFC University, Vadodara",
+    },
+    {
+        name: "Ms. Dharti Patel",
+        position: "Manager Facility, GSFC University, Vadodara",
+    },
+    {
+        name: "Ms. Sachiyanka Shrivastava",
+        position: "Dy. Manager - Marketing and PR, GSFC University, Vadodara",
+    },
+];
+
+export const EDITORIAL_TEAM: Array<Member> = [
+    {
+        name: "Prof. G. R Sinha",
+        position: "Vice - Chancellor GSFC University, Vadodara, India",
+    },
+    {
+        name: "Prof. Varun Bajaj",
+        position:
+            "Associate Professor, Maulana Azad National Institute of Technology, Bhopal, India",
+    },
+    {
+        name: "Dr.  Silvia Liberata Ullo",
+        position:
+            "Researcher, Université degli Studi del Sannio di Benevento, Italy",
+    },
+    {
+        name: "Prof. Humaira Nisar",
+        position: "Universiti Tunku Abdul Rahman, Kajang, Malaysia",
+    },
+];
